689c29e9bdee215ac8aa6e6b2cb1fb9cee1f438c
maxsendsen <74530318+maxsendsen@users.noreply.github.com>
2021-02-25 10:46:35 +0100

    tilføjet kommentarer

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+/*javascript er kopiret fra w3schools 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
